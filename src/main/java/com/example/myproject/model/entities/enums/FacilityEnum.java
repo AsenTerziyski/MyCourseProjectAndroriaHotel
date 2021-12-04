b74047856c3f7504d7185be86d2fa5b01d25781a
AsenTerziyski <asenterziyski@gmail.com>
2021-12-04 11:14:15 +0200

    delete

--- src/main/java/com/example/myproject/model/entities/enums/FacilityEnum.java
+++
@@ -1,5 +0,0 @@
-package com.example.myproject.model.entities.enums;
-
-public enum FacilityEnum {
-    DOUBLE_ROOM, APARTMENT, STUDIO, PARKING, RESTAURANT
-}
--- src/main/java/com/example/myproject/service/StatsService.java
+++
@@ -1,8 +0,0 @@
-package com.example.myproject.service;
-
-import com.example.myproject.model.view.StatsView;
-
-public interface StatsService {
-    void onRequest();
-    StatsView getStats();
-}
