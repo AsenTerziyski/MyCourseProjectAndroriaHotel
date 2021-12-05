0efa28343db9d9a6787aff06a212e84d4e419bc9
AsenTerziyski <asenterziyski@gmail.com>
2021-12-05 11:06:13 +0200

    delete

--- src/main/java/com/example/myproject/model/entities/enums/UserRoleEnum.java
+++
@@ -1,6 +0,0 @@
-package com.example.myproject.model.entities.enums;
-
-public enum UserRoleEnum {
-    USER,
-    ADMIN
-}
--- src/main/java/com/example/myproject/repository/ReviewTempRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.myproject.repository;
-
-import com.example.myproject.model.entities.ReviewTempEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReviewTempRepository extends JpaRepository<ReviewTempEntity, Long> {
-}
