c416f1510ad4bb737e1f6d8085a46e70392a67bc
AsenTerziyski <asenterziyski@gmail.com>
2021-12-10 09:02:44 +0200

    delete

--- src/main/java/com/example/myproject/service/ReviewTempService.java
+++
@@ -1,9 +0,0 @@
-package com.example.myproject.service;
-
-import com.example.myproject.model.entities.ReviewTempEntity;
-
-public interface ReviewTempService {
-    void deleteAllTempReviews();
-
-    void addTempReview(ReviewTempEntity reviewTempEntity);
-}
